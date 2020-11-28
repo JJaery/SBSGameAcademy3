8d1339bcc888a8758985afb731143af066876e3b
전인재 <jjaery@433.co.kr>
2020-11-29 04:37:35 +0900

    4주차 푸시

---
+++ 4주차/practice/New Unity Project/Assets/Scenes/SmoothMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMove : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        //StartCoroutine(CoMove());
+        Move();
+    }
+
+
+    IEnumerator CoMove()
+    {
+        int i = 0;
+
+        while (i++ < 10)
+        {
+            transform.position += Vector3.right;
+            yield return null;
+        }
+    }
+
+    void Move()
+    {
+        int i = 0;
+
+        while(i++ < 10)
+        {
+            transform.position += Vector3.right;
+        }
+    }
+}
