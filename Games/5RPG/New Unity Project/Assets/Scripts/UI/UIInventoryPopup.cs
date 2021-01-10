93c7e5bd2085ab2bcc2a82cf6c3b6bbd78995480
Jeon In Jae <jjaery@433.co.kr>
2021-01-10 18:11:27 +0900

    중간 저장

---
+++ Games/5RPG/New Unity Project/Assets/Scripts/UI/UIInventoryPopup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventoryPopup : MonoBehaviour
+{
+    public GameObject itemPrefab;
+
+    public GridLayoutGroup grid;
+
+    private void Awake()
+    {
+        for (int i = 0; i < InventoryManager.maxSlot; i++)
+        {
+            GameObject obj = Instantiate(itemPrefab, grid.transform);
+        }
+    }
+}
