319389e4fada28bb4c83c253c062b6321092f0ca
Jeon In Jae <jjaery@433.co.kr>
2021-01-17 16:04:51 +0900

    중간저장

---
+++ Games/5RPG/New Unity Project/Assets/Scripts/Character/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public Rigidbody rigidBody;
+    public Animator animator;
+
+    private void Update()
+    {
+        Vector3 dir = Vector3.zero;
+        if(Input.GetKey(KeyCode.W) == true) // 방향이 0도
+        {
+            dir += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A) == true) // 방향이 -90도
+        {
+            dir += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) == true) //방향이 90도
+        {
+            dir += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.S) == true) // 방향이 180도 설정
+        {
+            dir += Vector3.back;
+        }
+
+        ///이동
+        if (dir != Vector3.zero)
+        {
+            rigidBody.position += dir.normalized * moveSpeed * Time.deltaTime;
+            animator.transform.rotation = Quaternion.Euler(0, Vector3.SignedAngle(Vector3.forward, dir, Vector3.up), 0);
+            animator.SetBool("isMoving", true);
+        }
+        else // 이동 하지 않을 때 
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
